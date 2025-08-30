538e0322752587e10243eee8596391dbad64c1d7
Thamisousamacedo <thamisousa@hotmail.com>
2025-08-30 18:41:52 -0300

    feat: Coloca o Lombok nas Classes ImovelModel e PagamentoModel para abstração

--- src/main/java/how/projeto/CadastroDeImoveis/Imovel/ImovelModel.java
+++ src/main/java/how/projeto/CadastroDeImoveis/Imovel/ImovelModel.java
@@ -2,9 +2,15 @@ package how.projeto.CadastroDeImoveis.Imovel;
 
 import how.projeto.CadastroDeImoveis.Pagamentos.PagamentoModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_cadstro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ImovelModel {
 
     @Id
--- src/main/java/how/projeto/CadastroDeImoveis/Pagamentos/PagamentoModel.java
+++ src/main/java/how/projeto/CadastroDeImoveis/Pagamentos/PagamentoModel.java
@@ -3,12 +3,18 @@ package how.projeto.CadastroDeImoveis.Pagamentos;
 
 import how.projeto.CadastroDeImoveis.Imovel.ImovelModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
 @Table(name = "tb_pagamento")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PagamentoModel {
 
     @Id
