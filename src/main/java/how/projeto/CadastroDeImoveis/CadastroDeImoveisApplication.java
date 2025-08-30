6692493ee6d575cf6e98f0ab2f6870afb834c5da
Thamisousamacedo <thamisousa@hotmail.com>
2025-08-30 14:01:28 -0300

    first commit

---
+++ src/main/java/how/projeto/CadastroDeImoveis/CadastroDeImoveisApplication.java
@@ -0,0 +1,13 @@
+package how.projeto.CadastroDeImoveis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeImoveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeImoveisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/how/projeto/CadastroDeImoveis/CadastroDeImoveisApplicationTests.java
@@ -0,0 +1,13 @@
+package how.projeto.CadastroDeImoveis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeImoveisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
