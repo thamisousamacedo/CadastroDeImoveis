add0a1992054d1d133f95a724501d0955c29a015
Thamisousamacedo <thamisousa@hotmail.com>
2025-08-30 15:23:03 -0300

    feat: Criado a Controller

---
+++ src/main/java/how/projeto/CadastroDeImoveis/Controller.java
@@ -0,0 +1,15 @@
+package how.projeto.CadastroDeImoveis;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "Essa é a minha primeira mensagem nessa rota";
+    }
+}
