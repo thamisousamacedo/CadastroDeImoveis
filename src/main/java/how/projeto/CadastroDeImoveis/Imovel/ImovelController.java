417330ebaa3d519b2fb32584ea37800018f50317
Thamisousamacedo <thamisousa@hotmail.com>
2025-09-27 16:01:25 -0300

    refactor: Refatora o Controller para incluir o responseEntity nas minhas rotas e costumizar as rotas das requisições dos clientes

--- src/main/java/how/projeto/CadastroDeImoveis/Imovel/ImovelController.java
+++ src/main/java/how/projeto/CadastroDeImoveis/Imovel/ImovelController.java
@@ -1,5 +1,7 @@
 package how.projeto.CadastroDeImoveis.Imovel;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,35 +22,62 @@ public class ImovelController {
 
         // adicionar imovel (CREATE). @RequestBody manda no corpo da requisição o JSON que está no ImovelModel com os dados de id, tipo, descrição... toda vez que ele mandar o JSON, ele vai pegar o JSON serelializar e empurrar para salvar no BD
         @PostMapping("/criar")
-         public ImovelDTO criarImovel(@RequestBody ImovelDTO imovel) {
-            return imovelService.criarImovel(imovel);
+         public ResponseEntity<String> criarImovel(@RequestBody ImovelDTO imovel) {
+            ImovelDTO newImovel = imovelService.criarImovel(imovel);
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(("Imóvel criado com sucesso " + newImovel.getTipo() + " (ID) " + newImovel.getId()));
         }
 
 
         // mostrar todos os imoveis (READ). O Get está puxando do BD, serelializando em forma de JSON e mostrando para o usuário
         @GetMapping("/listar")
-        public List<ImovelDTO> listarImoveis() {
-            return imovelService.listarImoveis();
+        public ResponseEntity<List<ImovelDTO>> ListarImoveis() {
+            List<ImovelDTO> imoveis = imovelService.listarImoveis();
+            return ResponseEntity.ok(imoveis);
         }
 
 
         // mostrar imovel por id (READ). Puxando do BD, serealizando em forma de JSON. PathVariable pegar por id
         @GetMapping("/listar/{id}")
-        public ImovelDTO listarImovelPorId(@PathVariable long id) {return imovelService.ListarImoveisPorId(id);
+        public ResponseEntity<?> listarImovelPorId(@PathVariable long id) {
+
+            ImovelDTO imovel = imovelService.ListarImoveisPorId(id);
+
+            if (imovel != null) {
+                return ResponseEntity.ok(imovel);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body("Imóvel com o id: " + id + " não existe nos nossos registros!");
+            }
         }
 
         // alterar dados dos imoveis (UPDATE)
         @PutMapping("/alterar/{id}")
-        public ImovelDTO AlterarImovelPorId(@PathVariable Long id, @RequestBody ImovelDTO imovelAtualizado) {
-            return imovelService.alterarImovel(id, imovelAtualizado);
+        public ResponseEntity<?> AlterarImovelPorId(@PathVariable Long id, @RequestBody ImovelDTO imovelAtualizado) {
+            ImovelDTO imovel = imovelService.alterarImovel(id, imovelAtualizado);
+
+            if (imovel != null) {
+                return ResponseEntity.ok(imovel);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body("Imóvel com o id: " + id + " não existe nos nossos registros!");
+            }
         }
+
+
+
 
         // deletar imovel (DELETE)
         @DeleteMapping("/deletar/{id}")
-        public void deletarImovelPorId(@PathVariable Long id) {
-            imovelService.deletarImovelPorId(id);
+        public ResponseEntity<String> deletarImovelPorId(@PathVariable Long id) {
+            if (imovelService.ListarImoveisPorId(id) != null) {
+                imovelService.deletarImovelPorId(id);
+                return ResponseEntity.ok("Imóvel com o ID " + id + " deletado com sucesso!");
+
+            } else  {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body("O imóvel com o id " + id + " não encontrado!");
+            }
         }
-
-
 
 }
